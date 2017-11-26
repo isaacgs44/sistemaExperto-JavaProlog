53a423e5acebd542f779ce6aad726c78ceacde8a
isaacgs44 <isaacgs44@hotmail.com>
2017-11-25 22:13:33 -0600

    logica de prolog terminada, avance en interfáz

--- src/sistemaexpertomedico/Main.java
+++ src/sistemaexpertomedico/Main.java
@@ -5,7 +5,9 @@
  */
 package sistemaexpertomedico;
 
+import java.util.ArrayList;
 import java.util.Map;
+import javax.swing.JOptionPane;
 import org.jpl7.Query;
 import org.jpl7.Term;
 import org.jpl7.Util;
@@ -15,63 +17,67 @@ import org.jpl7.Variable;
  *
  * @author IsaacGS
  */
-public class Main extends javax.swing.JFrame {
+public class Main extends javax.swing.JFrame implements SistemaExperto.CallBackMain {
+
+    private SistemaExperto sistemaExp;
 
     /**
      * Creates new form Main
      */
     public Main() {
         initComponents();
-        String experto = "consult('expertojava.pl')";//aqui colocan el nombre de su archivo a compilar
-        String conocimiento = "consult('conocimiento.pl')";//aqui colocan el nombre de su archivo a compilar
+        sistemaExp = new SistemaExperto(this);
 
-        Query q1 = new Query(experto);
-        Query q2 = new Query(conocimiento);
-        System.out.println(experto + " " + (q1.hasSolution() ? "true" : "false")); //mostrara mensaje  si hay o no conexion           
-        System.out.println(conocimiento + " " + (q2.hasSolution() ? "true" : "false")); //mostrara mensaje  si hay o no conexion     
-
-        if (q1.hasSolution() && q2.hasSolution()) {
-            System.out.println("------------->Sistema Cargado correctamente");
-
-            Query q3 = new Query("conocimiento(X,Y)");
-            System.out.println("---Goal-->" + q3.goal());
-            leeConsulta(q3);
-        }else
-            System.out.println("--------------->El sistema experto no se pudo cargar");
+        if (sistemaExp.loadDataBase()) {
+            sistemaExp.startQuestions();
+        } else {
+            System.out.println("No se pudo cargar la base de conocimiento");
+        }
     }
 
-    private static void leeConsulta(Query q4) {
-        System.out.println("-------Goal----->" + q4.goal());
-        System.out.println("hasSol: " + (q4.hasSolution() ? "true" : "false"));
-        Map<String, Term>[] allSolutions = q4.allSolutions();
-        System.out.println("lenght solution: " + q4.allSolutions().length);
+    @Override
+    public void setQuestion(String question) {
+        lblPregunta.setText(question);
+    }
 
-        int j = 0;
-        while (q4.hasMoreSolutions()) {
+    @Override
+    public void diagnosisCompleted(String enfermedad) {
+        lblPregunta.setText("Enfermedad encontrada: " + enfermedad);
 
-            System.out.println("----->" + Util.toString(q4.nextSolution()));
+        String[] options = new String[]{"Aceptar", "Justificar diagnóstico"};
+        int response = JOptionPane.showOptionDialog(null, "La enfermedad encontrada es " + enfermedad, "Diagnóstico completo",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
 
-            System.out.println("j: " + j);
-            Map<String, Term> solution = q4.nextSolution();
-            System.out.println("X = " + solution.get("X"));
-            Term sintomas = solution.get("Y");
-            Term enfermedad = solution.get("X");
+        if (response == 1) {
+            ArrayList<String> sintomasEncontrados = sistemaExp.getSintomasEncontrados();
+            String message = "Se determino este diagnostico porque se encontraron los siguentes\n"
+                    + "sintomas: \n";
 
-            System.out.println("length: " + sintomas.toTermArray().length);
-            Term[] sintomasArray = sintomas.toTermArray();
-
-            for (int i = 0; i < sintomasArray.length; i++) {
-
-                System.out.println("--Argument--->" + sintomasArray[i]);
-                Query q5 = new Query("prueba_verdad_de(" + enfermedad.typeName()
-                        + ", " + sintomasArray[i].typeName() + ")");
-
-                System.out.println("-----Sol--->" + q5.hasSolution());
+            for (String s : sintomasEncontrados) {
+                message += "\n";
+                message += s;
             }
-
-            j++;
+            showDialog(message);
         }
+        closeDiagnosis();
+    }
 
+    @Override
+    public void finish() {
+        showDialog("No tengo suficiente conocimiento para diagnosticar esta enfermedad");
+        closeDiagnosis();
+    }
+
+    public void closeDiagnosis() {
+        Inicio iFrame = new Inicio();
+        iFrame.setVisible(true);
+        dispose();
+    }
+
+    @Override
+    public void showDialog(String message) {
+        JOptionPane.showMessageDialog(this, message);
     }
 
     /**
@@ -89,14 +95,40 @@ public class Main extends javax.swing.JFrame {
         lblPregunta = new javax.swing.JLabel();
         lblRespuesta = new javax.swing.JLabel();
         btnOk = new javax.swing.JButton();
+        btnCancel = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         radSi.setText("Si");
+        radSi.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                radSiStateChanged(evt);
+            }
+        });
+        radSi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                radSiActionPerformed(evt);
+            }
+        });
 
         radNo.setText("No");
+        radNo.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                radNoStateChanged(evt);
+            }
+        });
+        radNo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                radNoActionPerformed(evt);
+            }
+        });
 
         radPorQue.setText("Porque");
+        radPorQue.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                radPorQueStateChanged(evt);
+            }
+        });
         radPorQue.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 radPorQueActionPerformed(evt);
@@ -105,95 +137,171 @@ public class Main extends javax.swing.JFrame {
 
         lblPregunta.setText("Pregunta");
 
-        lblRespuesta.setText("Respuesta");
+        lblRespuesta.setText("Respuesta:");
 
-        btnOk.setText("Ok");
+        btnOk.setText("Siguiente");
+        btnOk.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOkActionPerformed(evt);
+            }
+        });
+
+        btnCancel.setText("Cancelar");
+        btnCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(67, 67, 67)
-                .addComponent(radSi)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(radNo)
-                .addGap(153, 153, 153)
-                .addComponent(radPorQue)
-                .addGap(70, 70, 70))
-            .addGroup(layout.createSequentialGroup()
+                .addGap(63, 63, 63)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblRespuesta, javax.swing.GroupLayout.PREFERRED_SIZE, 354, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(108, 108, 108)
-                        .addComponent(lblPregunta, javax.swing.GroupLayout.PREFERRED_SIZE, 354, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(100, 100, 100)
-                        .addComponent(lblRespuesta, javax.swing.GroupLayout.PREFERRED_SIZE, 354, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(260, 260, 260)
-                        .addComponent(btnOk)))
-                .addContainerGap(144, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(radSi)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(159, 159, 159)
+                                .addComponent(radNo))
+                            .addComponent(btnCancel))
+                        .addGap(97, 97, 97)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(radPorQue)
+                            .addComponent(btnOk)))
+                    .addComponent(lblPregunta, javax.swing.GroupLayout.PREFERRED_SIZE, 354, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(57, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(46, 46, 46)
+                .addGap(45, 45, 45)
                 .addComponent(lblPregunta)
-                .addGap(78, 78, 78)
+                .addGap(48, 48, 48)
+                .addComponent(lblRespuesta)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(radSi)
-                    .addComponent(radPorQue)
-                    .addComponent(radNo))
-                .addGap(31, 31, 31)
-                .addComponent(lblRespuesta)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
-                .addComponent(btnOk)
-                .addGap(20, 20, 20))
+                    .addComponent(radNo)
+                    .addComponent(radPorQue))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCancel)
+                    .addComponent(btnOk))
+                .addGap(41, 41, 41))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void radPorQueActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radPorQueActionPerformed
-        // TODO add your handling code here:
+        if (radPorQue.isSelected()) {
+            if (radNo.isSelected()) {
+                radNo.setSelected(false);
+            }
+            if (radSi.isSelected()) {
+                radSi.setSelected(false);
+            }
+        }
     }//GEN-LAST:event_radPorQueActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void btnOkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOkActionPerformed
+        String answer = "";
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Main().setVisible(true);
+        if (radSi.isSelected()) {
+            answer = "si";
+        } else if (radNo.isSelected()) {
+            answer = "no";
+        } else if (radPorQue.isSelected()) {
+            answer = "porque";
+        }
+
+        if (!answer.isEmpty()) {
+            sistemaExp.setAnswer(answer);
+            if (!sistemaExp.findDiagnostic()) {
+                sistemaExp.startQuestions();
             }
-        });
-    }
+        } else {
+            showDialog("Seleccione una respuesta");
+        }
+    }//GEN-LAST:event_btnOkActionPerformed
+
+    private void radNoStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_radNoStateChanged
+    }//GEN-LAST:event_radNoStateChanged
+
+    private void radSiStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_radSiStateChanged
+    }//GEN-LAST:event_radSiStateChanged
+
+    private void radPorQueStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_radPorQueStateChanged
+    }//GEN-LAST:event_radPorQueStateChanged
+
+    private void radNoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radNoActionPerformed
+        if (radNo.isSelected()) {
+            if (radSi.isSelected()) {
+                radSi.setSelected(false);
+            }
+            if (radPorQue.isSelected()) {
+                radPorQue.setSelected(false);
+            }
+        }
+    }//GEN-LAST:event_radNoActionPerformed
+
+    private void radSiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radSiActionPerformed
+        if (radSi.isSelected()) {
+            if (radNo.isSelected()) {
+                radNo.setSelected(false);
+            }
+            if (radPorQue.isSelected()) {
+                radPorQue.setSelected(false);
+            }
+        }
+    }//GEN-LAST:event_radSiActionPerformed
+
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+        closeDiagnosis();
+    }//GEN-LAST:event_btnCancelActionPerformed
+
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String args[]) {
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new Main().setVisible(true);
+//            }
+//        });
+//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancel;
     private javax.swing.JButton btnOk;
     private javax.swing.JLabel lblPregunta;
     private javax.swing.JLabel lblRespuesta;
